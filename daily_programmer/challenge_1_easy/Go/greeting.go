124e1d6cef12b0be509e79e93d9f6996d5001098
Fotis 'NlightNFotis' Koutoulakis <fotis.koutoulakis@gmail.com>
2014-06-19 16:12:29 +0300

    [TRIVIAL] Run gofmt on the go solution of the easy 1st challenge.

--- daily_programmer/challenge_1_easy/Go/greeting.go
+++ daily_programmer/challenge_1_easy/Go/greeting.go
@@ -1,36 +1,35 @@
 package main
 
 import (
-    "fmt"
-    //"io/ioutil"
-    "os"
+	"fmt"
+	"os"
 )
 
 func main() {
-    var name string
-    var age int
-    var reddit_username string
+	var name string
+	var age int
+	var reddit_username string
 
-    fmt.Println("What is your name?")
-    fmt.Scanln(&name)
+	fmt.Println("What is your name?")
+	fmt.Scanln(&name)
 
-    fmt.Println("What is your age?")
-    fmt.Scan(&age)
+	fmt.Println("What is your age?")
+	fmt.Scan(&age)
 
-    fmt.Println("What is your username in Reddit?")
-    fmt.Scanln(&reddit_username)
+	fmt.Println("What is your username in Reddit?")
+	fmt.Scanln(&reddit_username)
 
-    // create a format string to use for logging and stdoutput
-    resulting_string := fmt.Sprintf("Your name is %s, you are %d years old, and your username is %s",
-                        name, age, reddit_username)
-    
-    
-    fmt.Println(resulting_string)
+	// create a format string to use for logging and stdoutput
+	resulting_string := fmt.Sprintf("Your name is %s, you are %d years old, and your username is %s",
+		name, age, reddit_username)
 
-    // Write the resulting string to a log file.
-    f, err := os.Create("log.txt")
-    if err != nil { panic(err) }
-    f.WriteString(resulting_string)
-    defer f.Close()
+	fmt.Println(resulting_string)
+
+	// Write the resulting string to a log file.
+	f, err := os.Create("log.txt")
+	if err != nil {
+		panic(err)
+	}
+	f.WriteString(resulting_string)
+	defer f.Close()
 }
-
